ced74317290ad6a10d2b960e6fc3cf40ba83f5ef
rfan sgari <erfanasgari21@gmail.com>
2023-12-19 18:47:13 +0330

    top module completed

--- Accelerator.v
+++ Accelerator.v
@@ -1,13 +1,27 @@
 module Accelerator(
     input clk, rst, 
-    input wStart, lp,
+    input wStart,
     input [4:0] v,
     input [1:0] u,
 
     output wrReq, rdReq, wDone
-    output [20:0] wrData,
+    output [20:0] wrData
 );
 
-    WrapperController()
+    wire engDone, ldu, ldv, shL, engStart;
+    wire [15:0] engX;
+    wire [1:0] uOut;
+    wire [1:0] intpart;
+    wire [15:0] fracpart;
+
+    WrapperController controller(clk, rst, wStart, engDone, ldu, ldv, shL, engStart, wrReq, wDone);
+
+    ShiftRegisterLeft shRegL(clk, rst, shL, ldv, {3'b000, v, 8'b00000000}, engX);
+
+    Register2b uReg(clk, rst, ldu, u, uOut);
+
+    Exponential exp(clk, rst, engStart, engX, engDone, intpart, fracpart);
+
+    CombShifter combShifter({intpart, fracpart}, uOut, wrData);
 
 endmodule
--- Exponential.v
+++ Exponential.v
@@ -1,4 +1,4 @@
-module exponential(input clk,rst,start, input [15:0] x, 
+module Exponential(input clk,rst,start, input [15:0] x, 
 		   output done, output[1:0] intpart, output [15:0]fracpart);
 	
 	wire co,zx,initx,ldx,zt,initt,ldt,zr,initr,ldr,zc,ldc,enc,s;
